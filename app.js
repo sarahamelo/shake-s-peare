d85717711b9e1088042f56aa55ce5ba4f629854a
Jorge Terence <jorge.terence@outlook.com>
2024-09-25 20:25:09 +0000

    added CORS

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const booksRouter = require('./routes/booksRoutes');
 app.use('/books', booksRouter);
